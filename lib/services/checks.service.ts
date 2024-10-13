457cc0745a5323a557c8199416ea3cec6f7b50d6
Joaquin <hola@jrltt.net>
2024-10-13 18:20:00 +0200

    feat(checks service): add types

--- lib/services/checks.service.ts
+++ lib/services/checks.service.ts
@@ -2,7 +2,60 @@ import { getAccessToken } from "@/lib/services/auth.service";
 
 const baseURL = "https://uptime.com/api/v1/";
 
-export async function getChecks() {
+type Check = {
+  pk: number;
+  url: string;
+  stats_url: string;
+  alerts_url: string;
+  share_url: string;
+  name: string;
+  cached_response_time: number;
+  contact_groups: string[];
+  created_at: Date;
+  modified_at: Date;
+  locations: string[];
+  tags: string[];
+  check_type: string;
+  escalations: string;
+  maintenance: string;
+  monitoring_service_type: string;
+  is_paused: boolean;
+  is_under_maintenance: boolean;
+  state_is_up: boolean;
+  state_changed_at: Date;
+  msp_protocol: string;
+  msp_interval: number;
+  msp_address: string;
+  msp_port: number;
+  msp_username: string;
+  msp_proxy: string;
+  msp_dns_server: string;
+  msp_dns_record_type: string;
+  msp_status_code: string;
+  msp_send_string: string;
+  msp_expect_string: string;
+  msp_expect_string_type: string;
+  msp_encryption: string;
+  msp_threshold: number;
+  msp_headers: string;
+  msp_script: string;
+  msp_version: number;
+  msp_sensitivity: number;
+  msp_num_retries: number;
+  msp_use_ip_version: string;
+  msp_uptime_sla: string;
+  msp_response_time_sla: string;
+  msp_notes: string;
+  msp_include_in_global_metrics: boolean;
+};
+
+type CheckResults = {
+  count: number;
+  next: string;
+  previous: string;
+  results: Check[];
+};
+export async function getChecks(): Promise<CheckResults> {
   try {
     const result = await fetch(`${baseURL}checks/`, {
       headers: { Authorization: `Token ${getAccessToken()}` },
