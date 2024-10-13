d41e41de02833177693fc10d1c6fcf15ade64ee7
Joaquin <hola@jrltt.net>
2024-10-13 21:59:37 +0200

    feat(dashboard): add checks link

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -1,12 +1,20 @@
 "use client";
 
 import { logoutAction } from "@/actions/auth.action";
+import Link from "next/link";
 
 export function Dashboard() {
   return (
     <>
       <h1>Dashboard</h1>
-      <button onClick={() => logoutAction()}>Logout</button>
+      <ul>
+        <li>
+          <Link href="/dashboard/checks">Checks</Link>
+        </li>
+        <li>
+          <button onClick={() => logoutAction()}>Logout</button>
+        </li>
+      </ul>
     </>
   );
 }
