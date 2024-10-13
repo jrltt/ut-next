1105f2734fdba04c0024741124cd2371c2b30605
Joaquin <hola@jrltt.net>
2024-10-13 16:43:44 +0200

    feat(auth action): add action

---
+++ actions/auth.action.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import { login as loginService } from "@/lib/services/login.service";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function login(data: { email: string; password: string }) {
+  const response = await loginService(data);
+
+  if (response?.access_token) {
+    cookies().set("access_token", response.access_token);
+  }
+
+  redirect("/dashboard");
+}
+
+export async function logoutAction() {
+  cookies().delete("access_token");
+  redirect("/login");
+}
