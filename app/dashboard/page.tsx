b92d2293aa2cce086efa5ec6bb49095d61f9b416
Joaquin <hola@jrltt.net>
2024-10-14 10:57:47 +0200

    feat(dashboard page): split checks by type and render

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,68 @@
+import { CheckCard } from "@/components/check-card";
 import { Dashboard } from "@/components/dashboard";
+import { Check, getChecks } from "@/lib/services/checks.service";
+
+interface ChecksState {
+  downChecks: Check[];
+  pausedChecks: Check[];
+  activeChecks: Check[];
+  underMaintenanceChecks: Check[];
+}
+const checksReducer = (state: ChecksState, check: Check): ChecksState => {
+  if (!check.state_is_up) {
+    state.downChecks.push(check);
+  }
+  if (check.is_paused) {
+    state.pausedChecks.push(check);
+  } else {
+    state.activeChecks.push(check);
+  }
+  if (check.is_under_maintenance) {
+    state.underMaintenanceChecks.push(check);
+  }
+  return state;
+};
 
 export default async function DashboardPage() {
-  return <Dashboard />;
+  const checks = await getChecks();
+
+  const initialState = {
+    downChecks: [],
+    pausedChecks: [],
+    activeChecks: [],
+    underMaintenanceChecks: [],
+  };
+
+  const { downChecks, pausedChecks, activeChecks, underMaintenanceChecks } =
+    checks.results.reduce(checksReducer, initialState);
+
+  const checksMetrics = {
+    down: downChecks.length,
+    paused: pausedChecks.length,
+    maintenance: underMaintenanceChecks.length,
+  };
+
+  return (
+    <>
+      <Dashboard metrics={{ checks: checksMetrics }} />
+      <h4 className="text-2xl font-bold border-b border-b-slate-300 pb-6 mb-8">
+        Checks
+      </h4>
+      <div className="flex space-x-6">
+        {activeChecks.map((check) => (
+          <CheckCard
+            key={check.pk}
+            {...{
+              check: {
+                created_at: check.created_at,
+                msp_address: check.msp_address,
+                name: check.name,
+                pk: check.pk,
+              },
+            }}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
