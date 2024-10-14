7625002d1741866bca1400b32255bfd631f45b42
Joaquin <hola@jrltt.net>
2024-10-14 18:33:31 +0200

    feat(check action): clean up

--- actions/checks.action.ts
+++ actions/checks.action.ts
@@ -4,8 +4,7 @@ import { Check, patchCheck } from "@/lib/services/checks.service";
 import { redirect } from "next/navigation";
 
 export async function updateCheck(pk: number, check: Partial<Check>) {
-  const data = await patchCheck(pk, check);
-  console.log("patchCheck", data);
+  await patchCheck(pk, check);
 
-  redirect("/dashboard/checks");
+  redirect("/dashboard");
 }
