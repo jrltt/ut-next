b5d7342595f656b0005e820fae4a557322274a18
Joaquin <hola@jrltt.net>
2024-10-14 14:56:54 +0200

    feat(checks page): render all elements

--- app/dashboard/checks/page.tsx
+++ app/dashboard/checks/page.tsx
@@ -4,14 +4,13 @@ import { getChecks } from "@/lib/services/checks.service";
 export default async function ChecksPage() {
   const checks = await getChecks();
 
-  const activeChecks = checks.results.filter((check) => !check.is_paused);
-
   return (
     <>
-      <h1>Checks page</h1>
-      {/* <pre>{JSON.stringify(checks, null, 2)}</pre> */}
+      <h4 className="text-2xl font-bold border-b border-b-slate-300 pb-6 mb-8">
+        Checks
+      </h4>
       <div className="flex space-x-6">
-        {activeChecks.map((check) => (
+        {checks.results.map((check) => (
           <CheckCard key={check.pk} check={check} />
         ))}
       </div>
