2050324e2e67f0d8df6b70cf021d6ccc231aeee1
Joaquin <hola@jrltt.net>
2024-10-14 10:57:16 +0200

    feat(dashboard): implement metrics components

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -1,20 +1,45 @@
-"use client";
+import { ArrowRight } from "lucide-react";
+import { DashboardMetrics } from "./dashboard-metrics";
 
-import { logoutAction } from "@/actions/auth.action";
-import Link from "next/link";
-
-export function Dashboard() {
+interface Props {
+  metrics: {
+    checks: {
+      down: number;
+      paused: number;
+      maintenance: number;
+    };
+  };
+}
+export function Dashboard({ metrics }: Props) {
   return (
     <>
-      <h1>Dashboard</h1>
-      <ul>
-        <li>
-          <Link href="/dashboard/checks">Checks</Link>
-        </li>
-        <li>
-          <button onClick={() => logoutAction()}>Logout</button>
-        </li>
-      </ul>
+      <h1 className="text-3xl font-bold border-b border-b-slate-300 pb-6 mb-8">
+        Dashboard
+      </h1>
+      <div className="grid grid-cols-4 shadow-sm mb-12">
+        <div className="grid-span-1 bg-white w-full h-full p-4 border">
+          <h6 className="flex items-center font-bold mb-4">
+            Total checks <ArrowRight className="w-5 h-5 text-blue-700" />
+          </h6>
+          <ul className="space-y-3 text-slate-500">
+            <li>Down</li>
+            <li className="border-t border-slate-300 border-b py-3">Paused</li>
+            <li>Maintenance</li>
+          </ul>
+        </div>
+        <DashboardMetrics
+          title="Outages"
+          items={["Down", "Paused", "Maintenance"]}
+        />
+        <DashboardMetrics
+          title="Response Time"
+          items={["HTTP", "Transaction", "API"]}
+        />
+        <DashboardMetrics
+          title="Other"
+          items={["Global Uptime", "Last Alert", "RUM Load Time"]}
+        />
+      </div>
     </>
   );
 }
