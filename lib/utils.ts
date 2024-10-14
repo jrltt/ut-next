73d45abc28b24daadf9069fd165c64210c9a71be
Joaquin <hola@jrltt.net>
2024-10-14 14:56:03 +0200

    feat(utils): add label render by check type

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+// Available types:
+// HTTP, HTTP_POC, RUM2, ICMP, SSH, TCP, UDP, DNS, SMTP, POP, IMAP, NTP, BLACKLIST, WHOIS, MALWARE, SSL_CERT, TRANSACTION, API, HEARTBEAT, WEBHOOK, GROUP, PAGESPEED, CLOUDSTATUS
+export function renderLabelType(type: string) {
+  const availableTypes = {
+    WHOIS: "Whois/Domain Expiry",
+    HTTP: "HTTP(S)",
+    PAGESPEED: "Page speed",
+  };
+  return availableTypes[type as keyof typeof availableTypes] ?? "Unknown";
 }
